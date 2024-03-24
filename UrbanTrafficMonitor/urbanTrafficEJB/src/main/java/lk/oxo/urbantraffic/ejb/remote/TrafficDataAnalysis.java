2e52c1a913dbc4ce69d270f09d3a597fd2f01924
Shakith Nimsara <nimsara.shakith@gmail.com>
2024-03-24 09:37:29 +0530

    Change traffic pattern identifying logic

--- UrbanTrafficMonitor/urbanTrafficEJB/src/main/java/lk/oxo/urbantraffic/ejb/remote/TrafficDataAnalysis.java
+++ UrbanTrafficMonitor/urbanTrafficEJB/src/main/java/lk/oxo/urbantraffic/ejb/remote/TrafficDataAnalysis.java
@@ -1,22 +1,30 @@
 package lk.oxo.urbantraffic.ejb.remote;
 
 import lk.oxo.urbantraffic.ejb.util.Efficiency;
+import lk.oxo.urbantraffic.ejb.util.AnalyzedLevel;
+import lk.oxo.urbantraffic.ejb.util.RushHour;
 import lk.oxo.urbantraffic.ejb.util.TrafficLevel;
 import lk.oxo.urbantraffic.model.TrafficData;
 import lk.oxo.urbantraffic.model.TrafficZone;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public interface TrafficDataAnalysis {
     double calculateAverageSpeed();
+
     Map<LocalDate, List<TrafficData>> getTrafficDataByDate();
-    Map<LocalDateTime, List<TrafficData>> filterTrafficDataByRushHour();
-    Map<LocalDateTime, Double>  calculateAverageSpeedRushHour();
-    Map<LocalDateTime, TrafficLevel> analyzeTrafficLevelOnRushHour();
+
+//    Map<LocalDateTime, List<TrafficData>> filterTrafficDataByRushHour();
+Map<LocalDate, Map<RushHour, List<TrafficData>>> filterTrafficDataByRushHour();
+
+//    Map<LocalDateTime, Double> calculateAverageSpeedRushHour();
+Map<LocalDate, Map<RushHour, Double>> calculateAverageSpeedRushHour();
+
+    Map<LocalDate, Map<RushHour, AnalyzedLevel>> analyzeTrafficLevelOnRushHour();
+
     Map<LocalDate, Map<TrafficZone, List<TrafficData>>> getTrafficDataByDateAndZone();
+
     Map<LocalDate, Map<TrafficZone, Efficiency>> calculateUrbanMobilityEfficiency();
 }
